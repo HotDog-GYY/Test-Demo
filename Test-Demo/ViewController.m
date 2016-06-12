4079f0297cfe048496a489568d23b242dcb0aa19
GYY <634243370@qq.com>
2016-06-12 13:53:47 +0800

    测试

--- Test-Demo/ViewController.m
+++ Test-Demo/ViewController.m
@@ -10,6 +10,15 @@
 
 @interface ViewController ()
 
+@property (nonatomic, strong) UILabel *numLabel;
+
+@property (nonatomic, strong) UIButton *jianBtn;
+
+@property (nonatomic, strong) UIButton *jiaBtn;
+
+@property (nonatomic, assign) NSInteger numClick;
+
+
 @end
 
 @implementation ViewController
@@ -17,11 +26,76 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    [self.view addSubview:self.numLabel];
+    [self.view addSubview:self.jianBtn];
+    [self.view addSubview:self.jiaBtn];
+    
+    
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+
+- (void)jianBtnClick
+{
+    self.numClick--;
+    self.numLabel.text = [NSString stringWithFormat:@"%ld", (long)self.numClick];
 }
+
+- (void)jiaBtnClick
+{
+    self.numClick++;
+    self.numLabel.text = [NSString stringWithFormat:@"%ld", (long)self.numClick];
+}
+
+
+- (UILabel *)numLabel
+{
+    if (!_numLabel)
+    {
+        _numLabel = [[UILabel alloc] initWithFrame:CGRectMake(85, 70, 50, 30)];
+        _numLabel.text = @"0";
+        _numLabel.textAlignment = 1;
+        _numLabel.textColor = [UIColor redColor];
+        _numLabel.font = [UIFont systemFontOfSize:14];
+        _numLabel.backgroundColor = [UIColor cyanColor];
+        [self.view addSubview:_numLabel];
+        
+    }
+    return _numLabel;
+}
+
+
+- (UIButton *)jianBtn
+{
+    if (!_jianBtn)
+    {
+        _jianBtn = [UIButton buttonWithType:UIButtonTypeCustom];
+        _jianBtn.frame = CGRectMake(40, 120, 50, 30);
+        [_jianBtn setTitle:@"减少" forState:UIControlStateNormal];
+        [_jianBtn setTitleColor:[UIColor orangeColor] forState:UIControlStateNormal];
+        [_jianBtn addTarget:self action:@selector(jianBtnClick) forControlEvents:UIControlEventTouchUpInside];
+        [self.view addSubview:_jianBtn];
+        
+    }
+    return _jianBtn;
+}
+
+
+- (UIButton *)jiaBtn
+{
+    if (!_jiaBtn)
+    {
+        _jiaBtn = [UIButton buttonWithType:UIButtonTypeCustom];
+        _jiaBtn.frame = CGRectMake(130, 120, 50, 30);
+        [_jiaBtn setTitle:@"增加" forState:UIControlStateNormal];
+        [_jiaBtn setTitleColor:[UIColor orangeColor] forState:UIControlStateNormal];
+        [_jiaBtn addTarget:self action:@selector(jiaBtnClick) forControlEvents:UIControlEventTouchUpInside];
+        [self.view addSubview:_jiaBtn];
+        
+    }
+    return _jiaBtn;
+}
+
+
 
 @end
